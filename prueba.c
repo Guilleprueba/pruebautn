133e2fce9070838540f448d12e0b862e4ee9596f
Guilleprueba <carlosmiller11@hotmail.com>
2013-04-17 20:18:47 -0300

    Create prueba.c

---
+++ prueba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  int a,b,suma;
+	printf("Ingrese un valor:");
+	scanf("%d",&a);
+	printf("Ingrese un valor:");
+	scanf("%d",&b);
+	suma=a+b;
+	printf("La suma total es %d",suma);
+	return 0;
+}
